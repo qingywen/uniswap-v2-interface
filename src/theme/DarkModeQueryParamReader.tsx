8e70c208a25e3109d8e78035710921b8ba255150
qingywen <qingywen@gmail.com>
2022-03-09 14:28:40 +0800

    hide light mode

--- src/theme/DarkModeQueryParamReader.tsx
+++ src/theme/DarkModeQueryParamReader.tsx
@@ -22,7 +22,7 @@ export default function DarkModeQueryParamReader({ location: { search } }: Route
     if (typeof theme !== 'string') return
 
     if (theme.toLowerCase() === 'light') {
-      dispatch(updateUserDarkMode({ userDarkMode: false }))
+      dispatch(updateUserDarkMode({ userDarkMode: true }))
     } else if (theme.toLowerCase() === 'dark') {
       dispatch(updateUserDarkMode({ userDarkMode: true }))
     }
